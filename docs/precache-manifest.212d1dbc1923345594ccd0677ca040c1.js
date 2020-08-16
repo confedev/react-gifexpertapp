c948b250892ea903f66aef7c2c89c2ad441db0a4
Felipe Contreras <confedev@gmail.com>
2020-08-16 13:45:08 -0400

    docs agregado

---
+++ docs/precache-manifest.212d1dbc1923345594ccd0677ca040c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddf8722a3e9998fc3b0841073cee4fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82376ea42a6e103dff7d",
+    "url": "/static/css/main.3071439c.chunk.css"
+  },
+  {
+    "revision": "26227e71ad7d4f1513b6",
+    "url": "/static/js/2.d7cf9fae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7cf9fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82376ea42a6e103dff7d",
+    "url": "/static/js/main.02022d75.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
